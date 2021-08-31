6db94980cd91423e73340bced36eefd3cf0002f6
Amie Foster <amie.n.foster@gmail.com>
2021-08-31 00:09:59 -0600

    2 tests left

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Your code here
+
+saturdayFun();
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+
+
+function mondayWork(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(flair = '*'){
+    return function(status = 'special'){
+        return `You are ${status}`
+    }
+}
+
+const encouragingPromptFunction = wrapAdjective("!!!")
